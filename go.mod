9ec75184fca72515b58e0b34f20a14e4c94e62a1
Ben Wakeford <ben.jz.wakeford@gmail.com>
2025-02-07 19:35:34 -0500

    Add unit tests

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/prometheus/client_golang v1.20.5
+	github.com/stretchr/testify v1.9.0
 	github.com/wafer-bw/go-toolbox/graceful v0.2.4
 	golang.org/x/tools v0.29.0
 	google.golang.org/grpc v1.65.0
@@ -18,8 +19,11 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
@@ -29,4 +33,5 @@ require (
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/peer/jitter_test.go
+++ internal/peer/jitter_test.go
@@ -1,3 +1,230 @@
 package peer_test
 
-// TODO: this.
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/wafer-bw/jittermon/internal/peer"
+)
+
+func TestPeerRequestBuffers_Jitter(t *testing.T) {
+	t.Parallel()
+
+	t.Run("measures delayed jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(52 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 51*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2 * time.Second),
+			ReceivedAt: now.Add(2*time.Second + 54*time.Millisecond),
+		})
+
+		// Send Jitter: 0
+		// Receive Jitter:
+		// 51 - 52 = -1
+		// 54 - 51 = 3
+		// 3 - -1 = 4 (4ms delayed)
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 4*time.Millisecond, jitter)
+	})
+
+	t.Run("measures hastened jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(52 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 53*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2 * time.Second),
+			ReceivedAt: now.Add(2*time.Second + 48*time.Millisecond),
+		})
+
+		// Send Jitter: 0
+		// Receive Jitter:
+		// 53 - 52 = 1
+		// 48 - 53 = -5
+		// -5 - 1 = -6 (6ms hastened)
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 6*time.Millisecond, jitter)
+	})
+
+	t.Run("negates delayed send jitter from delayed receive jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(52 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 51*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2*time.Second + 2*time.Millisecond),
+			ReceivedAt: now.Add(2*time.Second + 54*time.Millisecond),
+		})
+
+		// Send Jitter:
+		// 1.000 - 0.000 = 1.000
+		// 2.002 - 1.000 = 1.002
+		// 1.002 - 1.000 = 0.002 (2ms delayed)
+		//
+		// Receive Jitter:
+		// 51 - 52 = -1
+		// 54 - 51 = 3
+		// 3 - -1 = 4 (4ms delayed)
+		//
+		// Negated:
+		// 4 - 2 = 2
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 2*time.Millisecond, jitter)
+	})
+
+	t.Run("negates hastened send jitter from hastened receive jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(50 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 50*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2*time.Second - 2*time.Millisecond),
+			ReceivedAt: now.Add(2*time.Second + 47*time.Millisecond),
+		})
+
+		// Send Jitter:
+		// 1.000 - 0.000 = 1.000
+		// 1.998 - 1.000 = 0.998
+		// 0.998 - 1.000 = -0.002 (2ms hastened)
+		//
+		// Receive Jitter:
+		// 50 - 50 = 0
+		// 47 - 50 = -3 (3ms hastened)
+		//
+		// Negated:
+		// -3 - -2 = |-1| -> 1
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 1*time.Millisecond, jitter)
+	})
+
+	t.Run("negates hastened send jitter from delayed receive jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(50 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 50*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2*time.Second - 2*time.Millisecond),
+			ReceivedAt: now.Add(2*time.Second + 54*time.Millisecond),
+		})
+
+		// Send Jitter:
+		// 1.000 - 0.000 = 1.000
+		// 1.998 - 1.000 = 0.998
+		// 0.998 - 1.000 = -0.002 (2ms hastened)
+		//
+		// Receive Jitter:
+		// 50 - 50 = 0
+		// 54 - 50 = 4
+		// 4 - 0 = 4 (4ms delayed)
+		//
+		// Negated:
+		// 4 - -2 = 6 (6ms delayed)
+		//
+		// If there was no send/receive jitter we would expect the 3rd packet to
+		// be received at 2.050, since the send came 2ms early we can adjust our
+		// expectatations such that it would be received at 2.048, but it
+		// actually came at 2.054, so it was 6ms delayed.
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 6*time.Millisecond, jitter)
+	})
+
+	t.Run("negates delayed send jitter from hastened receive jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := peer.PeerID("1")
+		now := time.Now()
+		b := peer.PeerRequestBuffers{}
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now,
+			ReceivedAt: now.Add(50 * time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(1 * time.Second),
+			ReceivedAt: now.Add(1*time.Second + 50*time.Millisecond),
+		})
+		b.Add(pid, peer.PeerRequest{
+			SentAt:     now.Add(2*time.Second + 2*time.Millisecond),
+			ReceivedAt: now.Add(2*time.Second + 46*time.Millisecond),
+		})
+
+		// Send Jitter:
+		// 1.000 - 0.000 = 1.000
+		// 2.002 - 1.000 = 1.002
+		// 1.002 - 1.000 = 0.002 (2ms delayed)
+		//
+		// Receive Jitter:
+		// 50 - 50 = 0
+		// 46 - 50 = -4
+		// -4 - 0 = -4 (4ms hastened)
+		//
+		// Negated:
+		// -4 - 2 =  -6 (6ms hastened)
+		//
+		// If there was no send/receive jitter we would expect the 3rd packet to
+		// be received at 2.050, since the send came 2ms late we can adjust our
+		// expectatations such that it would be received at 2.052, but it
+		// actually came at 2.046, so it was 6ms hastened.
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, 6*time.Millisecond, jitter)
+	})
+}
