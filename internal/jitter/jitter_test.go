f88bb3c2e3a5992eb9dbda3754c1b967c1855b8e
Ben Wakeford <ben.jz.wakeford@gmail.com>
2025-02-09 12:19:33 -0500

    test cleanup

--- internal/jitter/jitter_test.go
+++ internal/jitter/jitter_test.go
@@ -11,6 +11,27 @@ import (
 func TestPeerRequestBuffers_Jitter(t *testing.T) {
 	t.Parallel()
 
+	t.Run("measures zero jitter", func(t *testing.T) {
+		t.Parallel()
+
+		pid := "1"
+		now := time.Now()
+		b := jitter.HostPacketBuffers{}
+
+		b.Sample(pid, jitter.Packet{
+			S: now.Add(1 * time.Second),
+			R: now.Add(1*time.Second + 50*time.Millisecond),
+		})
+		b.Sample(pid, jitter.Packet{
+			S: now.Add(2 * time.Second),
+			R: now.Add(2*time.Second + 50*time.Millisecond),
+		})
+
+		jitter, ok := b.Jitter(pid)
+		require.True(t, ok)
+		require.Equal(t, time.Duration(0), jitter)
+	})
+
 	t.Run("measures delayed jitter", func(t *testing.T) {
 		t.Parallel()
 
@@ -95,7 +116,7 @@ func TestPeerRequestBuffers_Jitter(t *testing.T) {
 		require.Equal(t, 312500*time.Nanosecond, jitter)
 	})
 
-	t.Run("combines hastened send jitter from delayed receive jitter", func(t *testing.T) {
+	t.Run("combines hastened send jitter with delayed receive jitter", func(t *testing.T) {
 		t.Parallel()
 
 		pid := "1"
@@ -116,17 +137,13 @@ func TestPeerRequestBuffers_Jitter(t *testing.T) {
 		require.Equal(t, 937500*time.Nanosecond, jitter)
 	})
 
-	t.Run("combines delayed send jitter from hastened receive jitter", func(t *testing.T) {
+	t.Run("combines delayed send jitter with hastened receive jitter", func(t *testing.T) {
 		t.Parallel()
 
 		pid := "1"
 		now := time.Now()
 		b := jitter.HostPacketBuffers{}
 
-		b.Sample(pid, jitter.Packet{
-			S: now,
-			R: now.Add(50 * time.Millisecond),
-		})
 		b.Sample(pid, jitter.Packet{
 			S: now.Add(1 * time.Second),
 			R: now.Add(1*time.Second + 50*time.Millisecond),
