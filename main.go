4fafc54555eadba1fb26933df171fd3cb344c959
Ben Wakeford <ben.jz.wakeford@gmail.com>
2025-02-22 18:14:05 -0500

    Cleanup main

--- main.go
+++ main.go
@@ -16,30 +16,40 @@ import (
 const shutdownTimeout time.Duration = 250 * time.Millisecond
 
 type config struct {
-	PeerID      string                `split_words:"true"`
-	ListenAddr  string                `split_words:"true" default:":8080"`
-	SendAddrs   []string              `split_words:"true" default:":8081"`
-	MetricsAddr string                `split_words:"true" default:""`
-	Interval    time.Duration         `split_words:"true" default:"1s"`
-	LogLevel    slog.Level            `split_words:"true" default:"INFO"`
-	Metrics     []recorder.SampleType `split_words:"true" default:"rtt,downstream_jitter,upstream_jitter,sent_packets,lost_packets,hop_rtt"`
-	Write       bool                  `split_words:"true" default:"false"`
+	PeerID           string                `split_words:"true"`
+	ListenAddr       string                `split_words:"true" default:":8080"`
+	LatencySendAddrs []string              `split_words:"true" default:":8081"`
+	LatencyInterval  time.Duration         `split_words:"true" default:"1s"`
+	TraceSendAddrs   []string              `split_words:"true" default:""`
+	TraceInterval    time.Duration         `split_words:"true" default:"1s"`
+	TraceMaxHops     int                   `split_words:"true" default:"12"`
+	Metrics          []recorder.SampleType `split_words:"true" default:"rtt,downstream_jitter,upstream_jitter,sent_packets,lost_packets,hop_rtt"`
+	MetricsAddr      string                `split_words:"true" default:""`
+	LogLevel         slog.Level            `split_words:"true" default:"INFO"`
 }
 
 func main() {
 	ctx := context.Background()
-	conf := &config{}
-	envconfig.MustProcess("JITTERMON", conf)
+	conf := config{}
+	envconfig.MustProcess("JITTERMON", &conf)
+
+	log := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: conf.LogLevel}))
+
+	if err := run(ctx, log, conf); err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, log *slog.Logger, conf config) error {
 	group := graceful.Group{}
 	recorders := []recorder.ChainLink{recorder.MetricFilter(conf.Metrics...)}
-	log := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: conf.LogLevel}))
 	exitSignals := []os.Signal{syscall.SIGINT, syscall.SIGTERM}
 
 	if conf.MetricsAddr != "" {
 		prometheus, err := recorder.NewPrometheus(conf.MetricsAddr, log)
 		if err != nil {
-			log.Error(err.Error())
-			os.Exit(1)
+			return err
 		}
 		group = append(group, prometheus)
 		recorders = append(recorders, prometheus.DefaultRecorders()...)
@@ -47,56 +57,52 @@ func main() {
 
 	chain := recorder.Chain(recorders...)
 
-	// TODO: do we need the defers or can we move init portions into start functions?
-
-	// TODO: configure separately.
-	latencyClientSampler, err := sampler.NewLatencyClient(sampler.LatencyClientOptions{
-		ID:       conf.PeerID,
-		Address:  conf.SendAddrs[0],
-		Interval: conf.Interval,
-		Recorder: chain,
-		Log:      log,
-	})
-	if err != nil {
-		log.Error(err.Error())
-		return // TODO: put this in function so we can return proper exit code.
+	for _, addr := range conf.LatencySendAddrs {
+		latencyClientSampler, err := sampler.NewLatencyClient(sampler.LatencyClientOptions{
+			ID:       conf.PeerID,
+			Address:  addr,
+			Interval: conf.LatencyInterval,
+			Recorder: chain,
+			Log:      log,
+		})
+		if err != nil {
+			return err
+		}
+		group = append(group, latencyClientSampler)
 	}
-	defer latencyClientSampler.Stop(ctx) // TODO: will this panic if it ends up getting called twice?
-	group = append(group, latencyClientSampler)
 
-	// TODO: configure separately.
-	latencyServerSampler, err := sampler.NewLatencyServer(sampler.LatencyServerOptions{
-		ID:       conf.PeerID,
-		Address:  conf.ListenAddr,
-		Recorder: chain,
-		Log:      log,
-	})
-	if err != nil {
-		log.Error(err.Error())
-		return // TODO: put this in function so we can return proper exit code.
+	if conf.ListenAddr != "" {
+		latencyServerSampler, err := sampler.NewLatencyServer(sampler.LatencyServerOptions{
+			ID:       conf.PeerID,
+			Address:  conf.ListenAddr,
+			Recorder: chain,
+			Log:      log,
+		})
+		if err != nil {
+			return err
+		}
+		group = append(group, latencyServerSampler)
 	}
-	defer latencyServerSampler.Stop(ctx) // TODO: will this panic if it ends up getting called twice?
-	group = append(group, latencyServerSampler)
 
-	// TODO: configure separately.
-	traceRouteSampler, err := sampler.NewTraceRoute(sampler.TraceRouteOptions{
-		ID:       conf.PeerID,
-		Address:  conf.SendAddrs[0],
-		MaxHops:  12,                // TODO: configure.
-		Timeout:  conf.Interval * 4, // TODO: configure.
-		Interval: conf.Interval * 4, // TODO configure separately.
-		Recorder: chain,
-		Log:      log,
-	})
-	if err != nil {
-		log.Error(err.Error())
-		return // TODO: put this in function so we can return proper exit code.
+	for _, addr := range conf.TraceSendAddrs {
+		traceRouteSampler, err := sampler.NewTraceRoute(sampler.TraceRouteOptions{
+			ID:       conf.PeerID,
+			Address:  addr,
+			MaxHops:  conf.TraceMaxHops,
+			Timeout:  conf.TraceInterval,
+			Interval: conf.TraceInterval,
+			Recorder: chain,
+			Log:      log,
+		})
+		if err != nil {
+			return err
+		}
+		group = append(group, traceRouteSampler)
 	}
-	defer traceRouteSampler.Stop(ctx) // TODO: will this panic if it ends up getting called twice?
-	group = append(group, traceRouteSampler)
 
 	if err := group.Run(ctx, shutdownTimeout, exitSignals...); err != nil {
-		log.Error(err.Error())
-		return // TODO: put this in function so we can return proper exit code.
+		return err
 	}
+
+	return nil
 }
