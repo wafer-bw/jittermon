61f8cb1ecd01f334431dcd906cb544d5c00a2c8d
Ben Wakeford <ben.jz.wakeford@gmail.com>
2025-02-22 18:08:34 -0500

    Update tests & benchmarks for prometheus recorder

--- internal/recorder/prometheus_benchmark_test.go
+++ internal/recorder/prometheus_benchmark_test.go
@@ -4,8 +4,10 @@ package recorder_test
 // goarch: arm64
 // pkg: github.com/wafer-bw/jittermon/internal/recorder
 // cpu: Apple M2 Max
-// BenchmarkPrometheus_RecordDuration-12     	14746377	        81.02 ns/op	      32 B/op	       1 allocs/op
-// BenchmarkPrometheus_RecordIncrement-12    	18003543	        67.96 ns/op	      32 B/op	       1 allocs/op
+// BenchmarkPrometheus_RecordDuration/with_no_labels-12         	16667176	        74.23 ns/op	      32 B/op	       1 allocs/op
+// BenchmarkPrometheus_RecordDuration/with_2_labels-12          	11948625	        98.00 ns/op	      64 B/op	       2 allocs/op
+// BenchmarkPrometheus_RecordIncrement/with_no_labels-12        	19204582	        63.71 ns/op	      32 B/op	       1 allocs/op
+// BenchmarkPrometheus_RecordIncrement/with_2_labels-12         	13772055	        86.11 ns/op	      64 B/op	       2 allocs/op
 
 import (
 	"context"
@@ -17,7 +19,6 @@ import (
 
 func BenchmarkPrometheus_RecordDuration(b *testing.B) {
 	ctx := b.Context()
-	sample := recorder.Sample{Type: "abc", Src: "a", Dst: "b", Val: 1 * time.Second}
 
 	p, err := recorder.NewPrometheus(":8080", nil)
 	if err != nil {
@@ -26,14 +27,24 @@ func BenchmarkPrometheus_RecordDuration(b *testing.B) {
 
 	noop := recorder.RecorderFunc(func(context.Context, recorder.Sample) {})
 
-	for b.Loop() {
-		p.RecordDuration(noop).Record(ctx, sample)
-	}
+	b.Run("with no labels", func(b *testing.B) {
+		sample := recorder.Sample{Type: "abc", Val: 1 * time.Second}
+		for b.Loop() {
+			p.RecordDuration(noop).Record(ctx, sample)
+		}
+	})
+
+	b.Run("with 2 labels", func(b *testing.B) {
+		labels := recorder.Labels{{K: "a", V: "b"}, {K: "c", V: "d"}}
+		sample := recorder.Sample{Type: "def", Val: 1 * time.Second, Labels: labels}
+		for b.Loop() {
+			p.RecordDuration(noop).Record(ctx, sample)
+		}
+	})
 }
 
 func BenchmarkPrometheus_RecordIncrement(b *testing.B) {
 	ctx := b.Context()
-	sample := recorder.Sample{Type: "abc", Src: "a", Dst: "b", Val: struct{}{}}
 
 	p, err := recorder.NewPrometheus(":8080", nil)
 	if err != nil {
@@ -42,7 +53,18 @@ func BenchmarkPrometheus_RecordIncrement(b *testing.B) {
 
 	noop := recorder.RecorderFunc(func(context.Context, recorder.Sample) {})
 
-	for b.Loop() {
-		p.RecordIncrement(noop).Record(ctx, sample)
-	}
+	b.Run("with no labels", func(b *testing.B) {
+		sample := recorder.Sample{Type: "abc", Val: struct{}{}}
+		for b.Loop() {
+			p.RecordIncrement(noop).Record(ctx, sample)
+		}
+	})
+
+	b.Run("with 2 labels", func(b *testing.B) {
+		labels := recorder.Labels{{K: "a", V: "b"}, {K: "c", V: "d"}}
+		sample := recorder.Sample{Type: "def", Val: struct{}{}, Labels: labels}
+		for b.Loop() {
+			p.RecordIncrement(noop).Record(ctx, sample)
+		}
+	})
 }
